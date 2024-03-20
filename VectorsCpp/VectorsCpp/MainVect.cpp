e2ffa1d6c1f9447116efa8bfc958fbc3dab62723
Azot Eye <Azots@hotmail.com>
2024-03-20 15:38:24 +0300

    fix

--- VectorsCpp/VectorsCpp/MainVect.cpp
+++ VectorsCpp/VectorsCpp/MainVect.cpp
@@ -7,13 +7,13 @@ int main(int argc, char ** argv)
     std::ifstream in;
     char* infile, * outfile;
 
-    if (argc < 4)
+    if (argc < 3)
     {
         std::cout << "Wrong arguments";
         return 1;
     }
-    infile = argv[2];
-    outfile = argv[3];
+    infile = argv[1];
+    outfile = argv[2];
 
     Vect v1, v2;
 
